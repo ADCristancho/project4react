af169e50bf04f0ec9bbeeda3cd8e8c09b83f59a5
ADCristancho <ali3184533013@gmail.com>
2023-09-19 18:34:07 -0500

    toma9project4

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,31 +1,33 @@
+// UserCard.jsx
+
 function UserCard({ user, deleteUsers, setInfoUpdate, updateUsers, handleForm }) {
-
     const handleDelete = () => {
-    deleteUsers("/users", user.id);
+      deleteUsers("/users", user.id);
     };
-
+  
     const handleEdit = () => {
-    setInfoUpdate(user);
-    handleForm()
+      setInfoUpdate(user);
+      handleForm();
     };
-
-        return (
-    <article>
+  
+    return (
+      <article className="user-card"> {/* Agregar una clase user-card */}
         <h3>#{`${user.id} ${user.first_name} ${user.last_name}`}</h3>
-    <ul>
-        <li>
+        <ul>
+          <li>
             <span>Email: </span>
             <span>{user.email}</span>
-        </li>
-        <li>
+          </li>
+          <li>
             <span>Birthday: </span>
             <span>{user.birthday}</span>
-        </li>
-    </ul>
+          </li>
+        </ul>
         <button onClick={handleDelete}>Delete</button>
-    <button onClick={handleEdit}>Edit</button>
-    </article>
-);
-}
-
-export default UserCard;
+        <button onClick={handleEdit}>Edit</button>
+      </article>
+    );
+  }
+  
+  export default UserCard;
+  
